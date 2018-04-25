c0f294298f192790d2fc217db11b0f91235b9bd6
Carl-Erwin Griffith <carl.erwin@gmail.com>
2018-04-25 11:22:03 +0200

    run cargo fmt

--- src/core/editor.rs
+++ src/core/editor.rs
@@ -4,7 +4,6 @@ use std::cell::RefCell;
 use std::collections::HashMap;
 use std::thread;
 
-
 //
 use core;
 use ui;
@@ -40,7 +39,7 @@ pub type Id = u64;
 pub struct Editor<'a> {
     pub config: Config,
     pub document_map: HashMap<document::Id, Rc<RefCell<Document<'a>>>>,
-    pub view_map: Vec< (view::Id, Rc<RefCell<View<'a>>>) >,
+    pub view_map: Vec<(view::Id, Rc<RefCell<View<'a>>>)>,
 }
 
 impl<'a> Editor<'a> {
@@ -84,8 +83,6 @@ impl<'a> Editor<'a> {
             .internal(true);
 
         let b = builder.finalize();
-
-
 
         if let Some(b) = b {
             let id = self.document_map.len() as u64;
@@ -137,6 +134,5 @@ impl<'a> Editor<'a> {
         }
 
         println!("nb docs = {}", self.document_map.len());
-
     }
 }
