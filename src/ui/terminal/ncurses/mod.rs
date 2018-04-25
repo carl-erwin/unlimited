a9efbc7ece48aa051717ea0c196582b89b641802
Carl-Erwin Griffith <carl.erwin@gmail.com>
2018-04-25 16:35:31 +0200

    handle some keys (arrows, q)

--- src/ui/terminal/ncurses/mod.rs
+++ src/ui/terminal/ncurses/mod.rs
@@ -60,6 +60,12 @@ pub fn main_loop(mut editor: &mut Editor) {
 
     /* Start ncurses. */
     initscr();
+    curs_set(CURSOR_VISIBILITY::CURSOR_INVISIBLE);
+    keypad(stdscr(), true); /* for F1, arrow etc ... */
+    clear();
+    noecho();
+    raw();
+    halfdelay(1); // 20ms
 
     /* Get the screen bounds. */
     let mut width = 0;
@@ -75,20 +81,36 @@ pub fn main_loop(mut editor: &mut Editor) {
     while !ui_state.quit {
         ui_state.nb_view = editor.view_map.len();
         let mut view = Some(&editor.view_map[ui_state.vid as usize].1);
-
         getmaxyx(stdscr(), &mut height, &mut width);
+
+        // check screen size
+        {
+            let mut view = view.as_mut().unwrap().borrow_mut();
+            if width != ui_state.terminal_width as i32 || height != ui_state.terminal_height as i32
+            {
+                view.screen = Box::new(Screen::new(
+                    ui_state.terminal_width as usize,
+                    ui_state.terminal_height as usize,
+                ));
+            }
+        }
 
         if ui_state.display_view {
             draw_view(&mut ui_state, &mut view.as_mut().unwrap().borrow_mut());
         }
 
-        /* Wait for a key press. */
-        let key = getch();
-        if key == 'q' as i32 {
-            ui_state.quit = true;
-        }
+        let vec_evt = get_input_event(&mut ui_state);
+        for evt in vec_evt {
+            process_input_events(
+                &mut ui_state,
+                &mut view.as_mut().unwrap().borrow_mut(),
+                &evt,
+            );
 
-        //        fill_screen(&mut ui_state, &mut view.as_mut().unwrap().borrow_mut());
+            // re-sync view on each event/update or else the main mark will be offscreen
+            // TODO: add a view flag to call this
+            fill_screen(&mut ui_state, &mut view.as_mut().unwrap().borrow_mut());
+        }
     }
     /* Terminate ncurses. */
     endwin();
@@ -205,4 +227,375 @@ fn draw_screen(screen: &mut Screen, start_line: usize) {
 
     /* Update the screen. */
     refresh();
+}
+
+fn get_input_event(ui_state: &mut UiState) -> Vec<InputEvent> {
+    let mut v = Vec::<InputEvent>::new();
+
+    let keycode = wgetch(stdscr()); /* Wait for user input */
+    // FIXE: use timer to check core event
+    if keycode == ERR {
+        return v;
+    }
+
+    ui_state.status = format!(" keycode = {}", keycode);
+
+    let name = keyname(keycode);
+
+    match keycode {
+        KEY_UP => {
+            v.push(InputEvent::KeyPress {
+                ctrl: false,
+                alt: false,
+                shift: false,
+                key: Key::Up,
+            });
+        }
+
+        KEY_DOWN => {
+            v.push(InputEvent::KeyPress {
+                ctrl: false,
+                alt: false,
+                shift: false,
+                key: Key::Down,
+            });
+        }
+
+        KEY_LEFT => {
+            v.push(InputEvent::KeyPress {
+                ctrl: false,
+                alt: false,
+                shift: false,
+                key: Key::Left,
+            });
+        }
+        KEY_RIGHT => {
+            v.push(InputEvent::KeyPress {
+                ctrl: false,
+                alt: false,
+                shift: false,
+                key: Key::Right,
+            });
+        }
+
+        _ => {
+            /* TODO */
+
+            if keycode == 'q' as i32 {
+                ui_state.quit = true;
+            }
+        }
+    } // ! switch(keycode)
+
+    v
+}
+
+fn process_input_events(ui_state: &mut UiState, view: &mut View, ev: &InputEvent) {
+    if *ev == ::core::event::InputEvent::NoInputEvent {
+        // ignore no input event event :-)
+        return;
+    }
+
+    ui_state.keys.push(ev.clone());
+
+    let mut clear_keys = true;
+    match *ev {
+        InputEvent::KeyPress {
+            ctrl: true,
+            alt: false,
+            shift: false,
+            key: Key::UNICODE('q'),
+        } => {
+            ui_state.quit = true;
+        }
+
+        InputEvent::KeyPress {
+            ctrl: true,
+            alt: false,
+            shift: false,
+            key: Key::UNICODE('u'),
+        } => {
+            view.undo();
+        }
+
+        InputEvent::KeyPress {
+            ctrl: true,
+            alt: false,
+            shift: false,
+            key: Key::UNICODE('r'),
+        } => {
+            view.redo();
+        }
+
+        InputEvent::KeyPress {
+            ctrl: true,
+            alt: false,
+            shift: false,
+            key: Key::UNICODE('x'),
+        } => {
+            clear_keys = false;
+        }
+
+        // ctrl+a
+        InputEvent::KeyPress {
+            ctrl: true,
+            alt: false,
+            shift: false,
+            key: Key::UNICODE('a'),
+        }
+        | InputEvent::KeyPress {
+            ctrl: false,
+            alt: false,
+            shift: false,
+            key: Key::Home,
+        } => {
+            view.move_marks_to_beginning_of_line();
+        }
+
+        // ctrl+e
+        InputEvent::KeyPress {
+            ctrl: true,
+            alt: false,
+            shift: false,
+            key: Key::UNICODE('e'),
+        }
+        | InputEvent::KeyPress {
+            ctrl: false,
+            alt: false,
+            shift: false,
+            key: Key::End,
+        } => {
+            view.move_marks_to_end_of_line();
+        }
+
+        // ctrl+d
+        InputEvent::KeyPress {
+            ctrl: true,
+            alt: false,
+            shift: false,
+            key: Key::UNICODE('d'),
+        } => {
+            view.remove_codepoint();
+        }
+
+        // ctrl+s
+        InputEvent::KeyPress {
+            ctrl: true,
+            alt: false,
+            shift: false,
+            key: Key::UNICODE('s'),
+        } => {
+            view.save_document();
+        }
+
+        // ctrl+k
+        InputEvent::KeyPress {
+            ctrl: true,
+            alt: false,
+            shift: false,
+            key: Key::UNICODE('k'),
+        } => {
+            view.cut_to_end_of_line();
+        }
+
+        // ctrl+y
+        InputEvent::KeyPress {
+            ctrl: true,
+            alt: false,
+            shift: false,
+            key: Key::UNICODE('y'),
+        } => {
+            view.paste();
+        }
+
+        // ctrl+l
+        InputEvent::KeyPress {
+            ctrl: true,
+            alt: false,
+            shift: false,
+            key: Key::UNICODE('l'),
+        } => {
+            view.center_arround_mark();
+        }
+
+        // ctrl+?
+        InputEvent::KeyPress {
+            ctrl: true,
+            alt: false,
+            shift: false,
+            key: Key::UNICODE(c),
+        } => {
+            ui_state.status = format!("ctrl+<{}>", c);
+        }
+
+        // left
+        InputEvent::KeyPress {
+            ctrl: false,
+            alt: false,
+            shift: false,
+            key: Key::Left,
+        } => {
+            view.move_marks_backward();
+
+            ui_state.status = "<left>".to_owned();
+        }
+
+        // up
+        InputEvent::KeyPress {
+            ctrl: false,
+            alt: false,
+            shift: false,
+            key: Key::Up,
+        } => {
+            view.move_marks_to_previous_line();
+
+            ui_state.status = "<up>".to_owned();
+        }
+
+        // down
+        InputEvent::KeyPress {
+            ctrl: false,
+            alt: false,
+            shift: false,
+            key: Key::Down,
+        } => {
+            view.move_marks_to_next_line();
+
+            ui_state.status = "<down>".to_owned();
+        }
+
+        // right
+        InputEvent::KeyPress {
+            ctrl: false,
+            alt: false,
+            shift: false,
+            key: Key::Right,
+        } => {
+            view.move_marks_forward();
+
+            ui_state.status = "<right>".to_owned();
+        }
+
+        // page_up
+        InputEvent::KeyPress {
+            ctrl: false,
+            alt: false,
+            shift: false,
+            key: Key::PageUp,
+        } => {
+            view.scroll_to_previous_screen();
+            ui_state.status = "<page_up>".to_owned();
+        }
+
+        // page_down
+        InputEvent::KeyPress {
+            ctrl: false,
+            alt: false,
+            shift: false,
+            key: Key::PageDown,
+        } => {
+            view.scroll_to_next_screen();
+            ui_state.status = "<page_down>".to_owned();
+        }
+
+        // alt+< goto beginning of file
+        InputEvent::KeyPress {
+            ctrl: false,
+            alt: true,
+            shift: false,
+            key: Key::UNICODE('<'),
+        } => {
+            view.move_mark_to_beginning_of_file();
+            ui_state.status = "<move to beginning of file>".to_owned();
+        }
+
+        // alt+> goto end of file
+        InputEvent::KeyPress {
+            ctrl: false,
+            alt: true,
+            shift: false,
+            key: Key::UNICODE('>'),
+        } => {
+            view.move_mark_to_end_of_file();
+            ui_state.status = "<move to end of file>".to_owned();
+        }
+
+        // delete
+        InputEvent::KeyPress {
+            ctrl: false,
+            alt: false,
+            shift: false,
+            key: Key::Delete,
+        } => {
+            view.remove_codepoint();
+            ui_state.status = "<del>".to_owned();
+        }
+
+        // backspace
+        InputEvent::KeyPress {
+            ctrl: false,
+            alt: false,
+            shift: false,
+            key: Key::BackSpace,
+        } => {
+            view.remove_previous_codepoint();
+
+            ui_state.status = "<backspace>".to_owned();
+        }
+
+        // insert text
+        InputEvent::KeyPress {
+            ctrl: false,
+            alt: false,
+            shift: false,
+            key: Key::UNICODE(cp),
+        } => {
+            view.insert_codepoint(cp);
+            ui_state.status = format!("<insert [0x{:x}]>", cp as u32);
+        }
+
+        // mouse button pressed
+        InputEvent::ButtonPress {
+            ctrl: false,
+            alt: false,
+            shift: false,
+            x,
+            y,
+            button,
+        } => match button {
+            0 | 1 => {
+                view.button_press(button, x, y - 2);
+                ui_state.status = format!("<click({},@({},{}))]>", button, x, y - 2);
+            }
+            3 => {
+                view.scroll_up(3);
+                ui_state.status = format!("<click({},@({},{}))]>", button, x, y - 2);
+            }
+            4 => {
+                view.scroll_down(3);
+                ui_state.status = format!("<click({},@({},{}))]>", button, x, y - 2);
+            }
+
+            _ => {}
+        },
+
+        // mouse button released
+        InputEvent::ButtonRelease {
+            ctrl: false,
+            alt: false,
+            shift: false,
+            x,
+            y,
+            button,
+        } => {
+            view.button_release(button, x, y);
+            ui_state.status = format!("<unclick({},@({},{}))]>", button, x, y);
+        }
+
+        _ => {}
+    }
+
+    if clear_keys {
+        ui_state.keys.clear();
+    }
 }
