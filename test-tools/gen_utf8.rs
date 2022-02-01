551c86745c269f35db1985460eb8e1a82cfbb8f6
Carl-Erwin Griffith <carl.erwin@gmail.com>
2022-02-01 17:04:33 +0100

    fix gen_utf8

--- test-tools/gen_utf8.rs
+++ test-tools/gen_utf8.rs
@@ -2,44 +2,53 @@ use std::fs::File;
 use std::io::BufWriter;
 use std::io::Write;
 
+const SZ: usize = 1024 * 32;
+
 fn main() -> std::io::Result<()> {
     let file = File::create("all_utf8.bin")?;
 
-    let mut buf_writer = BufWriter::new(file);
+    let mut buf_writer = BufWriter::with_capacity(SZ, file);
 
-    for i in 0..0xff {
-        buf_writer.write(&[i])?;
+    let mut d: [u8; 256] = [0; 256];
+    for i in 0..256 {
+        d[i] = i as u8;
     }
+    buf_writer.write_all(&d)?;
 
-    for j in 0..0xff {
-        for i in 0..0xff {
-            buf_writer.write(&[i])?;
-            buf_writer.write(&[j])?;
+    let mut d: [u8; 256 * 2] = [0; 256 * 2];
+    for j in 0..256 {
+        for i in 0..256 {
+            d[(i * 2) + 0] = i as u8;
+            d[(i * 2) + 1] = j as u8;
         }
+        buf_writer.write_all(&d)?;
     }
 
-    for k in 0..0xff {
-        for j in 0..0xff {
-            for i in 0..0xff {
-                buf_writer.write(&[i])?;
-                buf_writer.write(&[j])?;
-                buf_writer.write(&[k])?;
+    let mut d: [u8; 256 * 3] = [0; 256 * 3];
+    for k in 0..256 {
+        for j in 0..256 {
+            for i in 0..256 {
+                d[(i * 3) + 0] = i as u8;
+                d[(i * 3) + 1] = j as u8;
+                d[(i * 3) + 2] = k as u8;
             }
+            buf_writer.write_all(&d)?;
         }
     }
 
-    for l in 0..0xff {
-        for k in 0..0xff {
-            for j in 0..0xff {
-                for i in 0..0xff {
-                    buf_writer.write(&[i])?;
-                    buf_writer.write(&[j])?;
-                    buf_writer.write(&[k])?;
-                    buf_writer.write(&[l])?;
+    let mut d: [u8; 256 * 4] = [0; 256 * 4];
+    for l in 0..256 {
+        for k in 0..256 {
+            for j in 0..256 {
+                for i in 0..256 {
+                    d[(i * 4) + 0] = i as u8;
+                    d[(i * 4) + 1] = j as u8;
+                    d[(i * 4) + 2] = k as u8;
+                    d[(i * 4) + 3] = l as u8;
                 }
+                buf_writer.write_all(&d)?;
             }
         }
     }
-
     Ok(())
 }
