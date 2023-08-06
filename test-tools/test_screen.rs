4e3ccb431db6f2a2bd1623a8b6d9b5cfab9f7f2d
Carl-Erwin Griffith <carl.erwin@gmail.com>
2023-08-06 12:13:55 +0200

    update screen test

--- test-tools/test_screen.rs
+++ test-tools/test_screen.rs
@@ -14,22 +14,59 @@ use std::time::Duration;
 use unlimited::core::codepointinfo::CodepointInfo;
 use unlimited::core::screen::*;
 
+extern crate unicode_width;
+use unicode_width::UnicodeWidthChar;
+
+struct Cell {
+    pub c: char,
+    pub real_c: char,
+    pub attr: u32,
+    pub fg: u32,
+    pub bg: u32,
+    pub off: Option<u64>,
+    pub size: u8,
+    pub cw: u8,
+}
+
+struct Screen2 {
+    pub cells: Vec<Cell>,
+    pub cur_index: usize,
+    pub cur_x: usize,
+    pub cur_y: usize,
+    pub width: usize,
+    pub height: usize,
+}
+
 fn main() -> std::io::Result<()> {
-    let mut buf = Vec::with_capacity(1024 * 64);
+    let os_args = env::args();
+    let args: Vec<_> = os_args.collect();
+
+    if args.len() != 4 {
+        println!("usage : {} [width] [height] [file]", args[0]);
+        return Ok(());
+    }
+
+    let width = args[1].trim_end().parse::<usize>().unwrap_or(128);
+    let height = args[2].trim_end().parse::<usize>().unwrap_or(32);
+
+    let mut buf = Vec::with_capacity(1024 * 32);
     buf.resize(buf.capacity(), 0);
 
-    let width = 350;
-    let height = 85;
+    let mut out_buf: Vec<Cell> = Vec::with_capacity(width * height);
     let mut screen = Screen::new(width, height);
 
     let mut fps = 0;
     let mut t0 = std::time::Instant::now();
 
-    for file in env::args().skip(1) {
+    println!("screen dimension {}x{}", width, height);
+
+    for file in env::args().skip(3) {
         println!("opening {}", file);
 
         let file = File::open(file).unwrap();
         let mut rbuf = BufReader::new(file);
+        let mut count: u64 = 0;
+        let mut total_frame: u64 = 0;
 
         loop {
             let rd_sz = rbuf.read(&mut buf[..]).unwrap();
@@ -38,17 +75,44 @@ fn main() -> std::io::Result<()> {
             }
 
             for c in &buf[..rd_sz] {
-                let mut cpi = CodepointInfo::new();
-                cpi.cp = *c as char;
-                cpi.displayed_cp = *c as char;
-                'retry: loop {
-                    let (ok, _line_index) = screen.push(cpi);
-                    if !ok {
-                        screen.clear();
+                if true {
+                    if out_buf.len() == out_buf.capacity() {
+                        out_buf.clear();
                         fps += 1;
-                        break 'retry;
+                        total_frame += 1;
                     }
-                    break;
+
+                    count += 1;
+
+                    let cw = UnicodeWidthChar::width(*c as char).unwrap_or(1) as u8;
+
+                    let cell = Cell {
+                        c: *c as char,
+                        real_c: (*c + 1) as char,
+                        attr: *c as u32,
+                        fg: (*c * 3) as u32,
+                        bg: (*c * 4) as u32,
+                        off: Some(count),
+                        size: 1,
+                        cw,
+                    };
+
+                    out_buf.push(cell);
+                    continue;
+                } else {
+                    let mut cpi = CodepointInfo::new();
+                    cpi.cp = *c as char;
+                    cpi.displayed_cp = *c as char;
+                    'retry: loop {
+                        let (ok, _line_index) = screen.push(cpi);
+                        if !ok {
+                            screen.clear();
+                            fps += 1;
+                            break 'retry;
+                        }
+                        break;
+                    }
+                    count += 1;
                 }
             }
 
@@ -59,6 +123,9 @@ fn main() -> std::io::Result<()> {
                 fps = 0;
             }
         }
+
+        println!("push count = {}", count);
+        println!("total_frame = {}", total_frame);
     }
 
     Ok(())
