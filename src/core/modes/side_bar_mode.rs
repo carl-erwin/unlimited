cdac630f6097c4a9be0a74f885a03c5854c9b805
Carl-Erwin Griffith <carl.erwin@gmail.com>
2024-05-07 17:47:05 +0200

    wip sidebar

--- src/core/modes/side_bar_mode.rs
+++ src/core/modes/side_bar_mode.rs
@@ -1,27 +1,23 @@
 use std::any::Any;
 
+use parking_lot::RwLock;
+use std::rc::Rc;
+
 use super::Mode;
 
+use crate::core::codepointinfo::CodepointInfo;
 use crate::core::editor::Editor;
 use crate::core::editor::EditorEnv;
 use crate::core::editor::InputStageActionMap;
 use crate::core::modes::text_mode::RawDataFilter;
 use crate::core::modes::text_mode::ScreenFilter;
-use crate::core::modes::text_mode::TabFilter;
-use crate::core::modes::text_mode::TextCodecFilter;
-use crate::core::modes::text_mode::UnicodeToTextFilter;
-use crate::core::modes::text_mode::Utf8Filter;
 use crate::core::view::View;
-
-use crate::core::screen::screen_apply;
 
 use crate::dbg_println;
 
 use crate::core::view::ContentFilter;
 
 use crate::core::view::LayoutEnv;
-
-use crate::core::codepointinfo::TextStyle;
 
 pub struct SideBarModeContext {}
 
@@ -58,47 +54,33 @@ impl<'a> Mode for SideBarMode {
         _env: &mut EditorEnv<'static>,
         view: &mut View<'static>,
     ) {
-        // let ctx = view.mode_ctx_mut::<SideBarModeContext>(self.name());
-
-        //
-        let use_utf8_codec = true;
-        let use_tabulation_exp = true;
-
         // mandatory data reader
         view.compose_content_filters
             .borrow_mut()
             .push(Box::new(RawDataFilter::new()));
-        //
 
-        if use_utf8_codec {
-            //
-            // DEBUG codec error
+        /*
+            if use_utf8_codec {
+                /* DEBUG codec error */
+                view.compose_content_filters
+                    .borrow_mut()
+                    .push(Box::new(Utf8Filter::new()));
+            }
+
             view.compose_content_filters
-                .borrow_mut()
-                .push(Box::new(Utf8Filter::new()));
-        } else {
-            view.compose_content_filters
-                .borrow_mut()
-                .push(Box::new(TextCodecFilter::new()));
-        }
-        //
-        view.compose_content_filters
             .borrow_mut()
             .push(Box::new(UnicodeToTextFilter::new()));
 
-        // TODO: char map 0x9 -> "\t"
-        if use_tabulation_exp {
-            view.compose_content_filters
-                .borrow_mut()
-                .push(Box::new(TabFilter::new()));
-        }
+        */
 
         let mut screen_filter = ScreenFilter::new();
         screen_filter.display_eof = false;
 
-        view.compose_content_filters
+        /*
+            view.compose_content_filters
             .borrow_mut()
             .push(Box::new(screen_filter));
+        */
 
         view.compose_content_filters
             .borrow_mut()
@@ -108,12 +90,15 @@ impl<'a> Mode for SideBarMode {
 
 pub struct SideBarModeCompose {
     // add common fields
+    buffer_list: Vec<String>,
 }
 
 impl SideBarModeCompose {
     pub fn new() -> Self {
         dbg_println!("SideBarMode");
-        SideBarModeCompose {}
+        SideBarModeCompose {
+            buffer_list: vec![],
+        }
     }
 
     pub fn register_input_stage_actions<'a>(_map: &'a mut InputStageActionMap<'a>) {}
@@ -124,13 +109,47 @@ impl ContentFilter<'_> for SideBarModeCompose {
         &"SideBarModeCompose"
     }
 
+    fn setup(
+        &mut self,
+        editor: &Editor<'static>,
+        _env: &mut LayoutEnv,
+        view: &Rc<RwLock<View>>,
+        _parent_view: Option<&View<'static>>,
+    ) {
+        // TODO(ceg): cache list
+        self.buffer_list.clear();
+
+        // get editor document list
+        for (id, b) in editor.buffer_map.read().iter() {
+            let b = b.read();
+            self.buffer_list.push(b.name.clone());
+        }
+
+        // order by cmd-line -> sort by buffer id order
+        // order by name
+        self.buffer_list.sort();
+    }
+
     fn finish(&mut self, _view: &View, env: &mut LayoutEnv) {
         // fill the whole status bar
-        screen_apply(&mut env.screen, |_, _, cpi| {
-            cpi.style.color = TextStyle::title_color();
-            cpi.style.bg_color = TextStyle::title_bg_color();
 
-            true
-        });
+        // TODO(ceg): add screen.println("")
+        // TODO(ceg): add screen.println_truncate("")
+
+        env.screen.clear();
+        let w = env.screen.width().saturating_sub(0);
+        for s in &self.buffer_list {
+            let slen = s.len();
+            for c in s.chars().take(w) {
+                let mut cpi = CodepointInfo::new();
+                cpi.displayed_cp = c;
+                cpi.cp = c;
+                env.screen.push(&cpi);
+                // push no newline
+            }
+            if slen < w {
+                env.screen.select_next_line_index();
+            }
+        }
     }
 }
